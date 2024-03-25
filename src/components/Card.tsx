af90bf3834d0b7efe77f72b54528e04f8ed3d768
Michal <michalgrzegorczyk04@gmail.com>
2024-03-25 10:45:55 +0100

    add Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+/** @format */
+
+interface CardProps {
+  children: string | JSX.Element | JSX.Element[];
+  bg?: string;
+}
+
+const Card = ({ children, bg = "bg-gray-100" }: CardProps) => {
+  return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
+};
+
+export default Card;
