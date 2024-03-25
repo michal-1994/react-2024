c7ee98bc47532ec7667792db9e166216e744f602
Michal <michalgrzegorczyk04@gmail.com>
2024-03-25 11:29:37 +0100

    add ViewAllJobs component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
 import Navbar from "./components/Navbar";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
   return (
@@ -14,15 +15,7 @@ const App = () => {
       />
       <HomeCards />
       <JobListings />
-
-      <section className='m-auto max-w-lg my-10 px-6'>
-        <a
-          href='jobs.html'
-          className='block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700'
-        >
-          View All Jobs
-        </a>
-      </section>
+      <ViewAllJobs />
     </>
   );
 };
