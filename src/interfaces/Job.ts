9c9ebf56760a6e978a489a80a509f9eb02ad315b
Michal <michalgrzegorczyk04@gmail.com>
2024-03-25 11:09:32 +0100

    split Job component

---
+++ src/interfaces/Job.ts
@@ -0,0 +1,18 @@
+/** @format */
+
+export interface Job {
+  id: string;
+  title: string;
+  type: string;
+  description: string;
+  location: string;
+  salary: string;
+  company: JobCompany;
+}
+
+interface JobCompany {
+  name: string;
+  description: string;
+  contactEmail: string;
+  contactPhone: string;
+}
