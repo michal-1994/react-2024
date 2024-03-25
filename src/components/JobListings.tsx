8fd62bf3523af3f39f9619fb5af86b2085b2eea9
Michal <michalgrzegorczyk04@gmail.com>
2024-03-25 11:28:07 +0100

    get 3 recent jobs

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -5,6 +5,8 @@ import jobs from "../jobs.json";
 import JobListing from "./JobListing";
 
 const JobListings = () => {
+  const recentJobs = jobs.slice(0, 3);
+
   return (
     <section className='bg-blue-50 px-4 py-10'>
       <div className='container-xl lg:container m-auto'>
@@ -12,7 +14,7 @@ const JobListings = () => {
           Browse Jobs
         </h2>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-          {jobs.map((job: Job) => (
+          {recentJobs.map((job: Job) => (
             <JobListing key={job.id} job={job} />
           ))}
         </div>
