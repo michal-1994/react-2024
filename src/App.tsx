3e8c46cfd1cbc4bd0ae04622effd07950e450004
Michal <michalgrzegorczyk04@gmail.com>
2024-03-25 14:10:57 +0100

    add job loader

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,14 @@ import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import MainLayout from "./layouts/MainLayout";
 import NotFoundPage from "./pages/NotFoundPage";
-import JobPage from "./pages/JobPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} />
+      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
       <Route path='/add-job' element={<AddJobPage />} />
       <Route path='*' element={<NotFoundPage />} />
     </Route>
--- src/pages/JobPage.tsx
+++ src/pages/JobPage.tsx
@@ -1,32 +1,15 @@
 /** @format */
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
-import Spinner from "../components/Spinner";
-import { Job } from "../interfaces/Job";
-
-const JobPage = () => {
-  const { id } = useParams();
-  const [job, setJob] = useState<Job | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const fetchJob = async () => {
-      try {
-        const res = await fetch(`/api/jobs/${id}`);
-        const data = await res.json();
-        setJob(data);
-      } catch (err) {
-        console.error(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchJob();
-  }, []);
-
-  return loading ? <Spinner loading={loading} /> : <h1>{job?.title}</h1>;
+const jobLoader = async ({ params }: any) => {
+  const res = await fetch(`/api/jobs/${params.id}`);
+  return await res.json();
 };
 
-export default JobPage;
+const JobPage = () => {
+  const job: any = useLoaderData();
+
+  return <h1>{job?.title}</h1>;
+};
+
+export { JobPage as default, jobLoader };
